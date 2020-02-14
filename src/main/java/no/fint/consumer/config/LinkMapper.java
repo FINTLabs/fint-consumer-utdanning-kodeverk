d693f2355ae4f4b43f8abfd8847bae4f50573112
Asgeir Nilsen <asgeir@fintlabs.no>
2020-02-14 09:59:06 +0100

    Updated LinkMapper

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -1,21 +1,25 @@
 package no.fint.consumer.config;
 
-import no.fint.consumer.utils.RestEndpoints;
-import java.util.Map;
 import com.google.common.collect.ImmutableMap;
+import no.fint.consumer.utils.RestEndpoints;
+import no.fint.model.utdanning.kodeverk.Elevkategori;
+import no.fint.model.utdanning.kodeverk.Fravarstype;
+import no.fint.model.utdanning.kodeverk.Karakterskala;
+import no.fint.model.utdanning.kodeverk.Skoleeiertype;
+import no.fint.model.utdanning.vurdering.Karakterverdi;
 
-import no.fint.model.utdanning.kodeverk.*;
+import java.util.Map;
 
 public class LinkMapper {
 
-	public static Map<String, String> linkMapper(String contextPath) {
-		return ImmutableMap.<String,String>builder()
-			.put(Elevkategori.class.getName(), contextPath + RestEndpoints.ELEVKATEGORI)
-			.put(Fravarstype.class.getName(), contextPath + RestEndpoints.FRAVARSTYPE)
-			.put(Karakterskala.class.getName(), contextPath + RestEndpoints.KARAKTERSKALA)
-			.put(Skoleeiertype.class.getName(), contextPath + RestEndpoints.SKOLEEIERTYPE)
-			/* .put(TODO,TODO) */
-			.build();
-	}
+    public static Map<String, String> linkMapper(String contextPath) {
+        return ImmutableMap.<String, String>builder()
+                .put(Elevkategori.class.getName(), contextPath + RestEndpoints.ELEVKATEGORI)
+                .put(Fravarstype.class.getName(), contextPath + RestEndpoints.FRAVARSTYPE)
+                .put(Karakterskala.class.getName(), contextPath + RestEndpoints.KARAKTERSKALA)
+                .put(Skoleeiertype.class.getName(), contextPath + RestEndpoints.SKOLEEIERTYPE)
+				.put(Karakterverdi.class.getName(), "/utdanning/vurdering/karakterverdi")
+                .build();
+    }
 
 }
