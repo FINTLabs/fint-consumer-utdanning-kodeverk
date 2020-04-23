60e32df03be42dfc7f7bfe90bfbbecc850baf9a0
Asgeir Storesund Nilsen <asgeir@fintlabs.no>
2020-04-23 08:31:43 +0200

    Version 3.4.5. (#4)
    * Version 3.4.5.
    
    * Remove k8s manifests, ref FINTLabs/fint-kubernetes#2

--- src/main/java/no/fint/consumer/models/elevkategori/ElevkategoriLinker.java
+++ src/main/java/no/fint/consumer/models/elevkategori/ElevkategoriLinker.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.Objects.isNull;
 import static org.springframework.util.StringUtils.isEmpty;
-
 
 @Component
 public class ElevkategoriLinker extends FintLinker<ElevkategoriResource> {
@@ -34,11 +34,17 @@ public class ElevkategoriLinker extends FintLinker<ElevkategoriResource> {
 
     @Override
     public String getSelfHref(ElevkategoriResource elevkategori) {
+        return getAllSelfHrefs(elevkategori).findFirst().orElse(null);
+    }
+
+    @Override
+    public Stream<String> getAllSelfHrefs(ElevkategoriResource elevkategori) {
+        Stream.Builder<String> builder = Stream.builder();
         if (!isNull(elevkategori.getSystemId()) && !isEmpty(elevkategori.getSystemId().getIdentifikatorverdi())) {
-            return createHrefWithId(elevkategori.getSystemId().getIdentifikatorverdi(), "systemid");
+            builder.add(createHrefWithId(elevkategori.getSystemId().getIdentifikatorverdi(), "systemid"));
         }
         
-        return null;
+        return builder.build();
     }
 
     int[] hashCodes(ElevkategoriResource elevkategori) {
--- src/main/java/no/fint/consumer/models/karakterskala/KarakterskalaLinker.java
+++ src/main/java/no/fint/consumer/models/karakterskala/KarakterskalaLinker.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.Objects.isNull;
 import static org.springframework.util.StringUtils.isEmpty;
-
 
 @Component
 public class KarakterskalaLinker extends FintLinker<KarakterskalaResource> {
@@ -34,11 +34,17 @@ public class KarakterskalaLinker extends FintLinker<KarakterskalaResource> {
 
     @Override
     public String getSelfHref(KarakterskalaResource karakterskala) {
+        return getAllSelfHrefs(karakterskala).findFirst().orElse(null);
+    }
+
+    @Override
+    public Stream<String> getAllSelfHrefs(KarakterskalaResource karakterskala) {
+        Stream.Builder<String> builder = Stream.builder();
         if (!isNull(karakterskala.getSystemId()) && !isEmpty(karakterskala.getSystemId().getIdentifikatorverdi())) {
-            return createHrefWithId(karakterskala.getSystemId().getIdentifikatorverdi(), "systemid");
+            builder.add(createHrefWithId(karakterskala.getSystemId().getIdentifikatorverdi(), "systemid"));
         }
         
-        return null;
+        return builder.build();
     }
 
     int[] hashCodes(KarakterskalaResource karakterskala) {
