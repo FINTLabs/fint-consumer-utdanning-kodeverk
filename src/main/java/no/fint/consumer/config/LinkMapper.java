2206f16fe4e633495aa19896c6e04fe423bac6b1
Asgeir Nilsen <asgeir@fintlabs.no>
2020-05-07 14:17:41 +0200

    Updated LinkMapper

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -2,10 +2,7 @@ package no.fint.consumer.config;
 
 import com.google.common.collect.ImmutableMap;
 import no.fint.consumer.utils.RestEndpoints;
-import no.fint.model.utdanning.kodeverk.Elevkategori;
-import no.fint.model.utdanning.kodeverk.Fravarstype;
-import no.fint.model.utdanning.kodeverk.Karakterskala;
-import no.fint.model.utdanning.kodeverk.Skoleeiertype;
+import no.fint.model.utdanning.kodeverk.*;
 import no.fint.model.utdanning.vurdering.Karakterverdi;
 
 import java.util.Map;
@@ -18,6 +15,8 @@ public class LinkMapper {
                 .put(Fravarstype.class.getName(), contextPath + RestEndpoints.FRAVARSTYPE)
                 .put(Karakterskala.class.getName(), contextPath + RestEndpoints.KARAKTERSKALA)
                 .put(Skoleeiertype.class.getName(), contextPath + RestEndpoints.SKOLEEIERTYPE)
+                .put(Skolear.class.getName(), contextPath + RestEndpoints.SKOLEAR)
+                .put(Termin.class.getName(), contextPath + RestEndpoints.TERMIN)
 				.put(Karakterverdi.class.getName(), "/utdanning/vurdering/karakterverdi")
                 .build();
     }
