22c62ac355f1971be2e90a2c5ec6eeeb33899d58
Ole Anders Eidjord <26850+oleanders@users.noreply.github.com>
2022-09-07 14:02:26 +0200

    Version 3.11.0. (#14)

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -4,10 +4,12 @@ import no.fint.consumer.utils.RestEndpoints;
 import java.util.Map;
 import com.google.common.collect.ImmutableMap;
 import no.fint.model.utdanning.kodeverk.Avbruddsarsak;
+import no.fint.model.utdanning.kodeverk.Eksamensform;
 import no.fint.model.utdanning.kodeverk.Elevkategori;
 import no.fint.model.utdanning.kodeverk.Fagmerknad;
 import no.fint.model.utdanning.kodeverk.Fravarstype;
 import no.fint.model.utdanning.kodeverk.Karakterskala;
+import no.fint.model.utdanning.kodeverk.Karakterstatus;
 import no.fint.model.utdanning.kodeverk.Skolear;
 import no.fint.model.utdanning.kodeverk.Skoleeiertype;
 import no.fint.model.utdanning.kodeverk.Termin;
@@ -18,10 +20,12 @@ public class LinkMapper {
     public static Map<String, String> linkMapper(String contextPath) {
         return ImmutableMap.<String,String>builder()
             .put(Avbruddsarsak.class.getName(), contextPath + RestEndpoints.AVBRUDDSARSAK)
+            .put(Eksamensform.class.getName(), contextPath + RestEndpoints.EKSAMENSFORM)
             .put(Elevkategori.class.getName(), contextPath + RestEndpoints.ELEVKATEGORI)
             .put(Fagmerknad.class.getName(), contextPath + RestEndpoints.FAGMERKNAD)
             .put(Fravarstype.class.getName(), contextPath + RestEndpoints.FRAVARSTYPE)
             .put(Karakterskala.class.getName(), contextPath + RestEndpoints.KARAKTERSKALA)
+            .put(Karakterstatus.class.getName(), contextPath + RestEndpoints.KARAKTERSTATUS)
             .put(Skolear.class.getName(), contextPath + RestEndpoints.SKOLEAR)
             .put(Skoleeiertype.class.getName(), contextPath + RestEndpoints.SKOLEEIERTYPE)
             .put(Termin.class.getName(), contextPath + RestEndpoints.TERMIN)
--- src/main/java/no/fint/consumer/utils/RestEndpoints.java
+++ src/main/java/no/fint/consumer/utils/RestEndpoints.java
@@ -5,10 +5,12 @@ public enum RestEndpoints {
 
     public static final String ADMIN = "/admin";
 	public static final String AVBRUDDSARSAK = "/avbruddsarsak";
+	public static final String EKSAMENSFORM = "/eksamensform";
 	public static final String ELEVKATEGORI = "/elevkategori";
 	public static final String FAGMERKNAD = "/fagmerknad";
 	public static final String FRAVARSTYPE = "/fravarstype";
 	public static final String KARAKTERSKALA = "/karakterskala";
+	public static final String KARAKTERSTATUS = "/karakterstatus";
 	public static final String SKOLEAR = "/skolear";
 	public static final String SKOLEEIERTYPE = "/skoleeiertype";
 	public static final String TERMIN = "/termin";
