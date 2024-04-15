6ff8cc6eb0ed3b66b3e567890164fd6c56f3fa53
Sander Nilsen Rysjedal <117441169+Sander14121@users.noreply.github.com>
2024-04-15 14:08:59 +0200

    new fint-relations (#22)

--- src/main/java/no/fint/consumer/models/fagstatus/FagstatusCacheService.java
+++ src/main/java/no/fint/consumer/models/fagstatus/FagstatusCacheService.java
@@ -107,7 +107,10 @@ public class FagstatusCacheService extends CacheService<FagstatusResource> {
         } else {
             data = objectMapper.convertValue(event.getData(), javaType);
         }
-        data.forEach(linker::mapLinks);
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
         if (KodeverkActions.valueOf(event.getAction()) == KodeverkActions.UPDATE_FAGSTATUS) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<FagstatusResource>> cacheObjects = data
--- src/main/java/no/fint/consumer/models/karakterstatus/KarakterstatusCacheService.java
+++ src/main/java/no/fint/consumer/models/karakterstatus/KarakterstatusCacheService.java
@@ -107,7 +107,10 @@ public class KarakterstatusCacheService extends CacheService<KarakterstatusResou
         } else {
             data = objectMapper.convertValue(event.getData(), javaType);
         }
-        data.forEach(linker::mapLinks);
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
         if (KodeverkActions.valueOf(event.getAction()) == KodeverkActions.UPDATE_KARAKTERSTATUS) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<KarakterstatusResource>> cacheObjects = data
