e571164039e7c951a0c1a34899c42272af1cbb2b
Henrik Knutsen <hknut99@gmail.com>
2024-05-21 13:55:28 +0200

    Version 3.17.1. (#23)
    * Version 3.17.1.
    
    * manual changes

---
+++ src/main/java/no/fint/consumer/models/brevtype/BrevtypeLinker.java
@@ -0,0 +1,64 @@
+package no.fint.consumer.models.brevtype;
+
+import no.fint.model.resource.utdanning.kodeverk.BrevtypeResource;
+import no.fint.model.resource.utdanning.kodeverk.BrevtypeResources;
+import no.fint.relations.FintLinker;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.Objects.isNull;
+import static org.springframework.util.StringUtils.isEmpty;
+
+@Component
+public class BrevtypeLinker extends FintLinker<BrevtypeResource> {
+
+    public BrevtypeLinker() {
+        super(BrevtypeResource.class);
+    }
+
+    public void mapLinks(BrevtypeResource resource) {
+        super.mapLinks(resource);
+    }
+
+    @Override
+    public BrevtypeResources toResources(Collection<BrevtypeResource> collection) {
+        return toResources(collection.stream(), 0, 0, collection.size());
+    }
+
+    @Override
+    public BrevtypeResources toResources(Stream<BrevtypeResource> stream, int offset, int size, int totalItems) {
+        BrevtypeResources resources = new BrevtypeResources();
+        stream.map(this::toResource).forEach(resources::addResource);
+        addPagination(resources, offset, size, totalItems);
+        return resources;
+    }
+
+    @Override
+    public String getSelfHref(BrevtypeResource brevtype) {
+        return getAllSelfHrefs(brevtype).findFirst().orElse(null);
+    }
+
+    @Override
+    public Stream<String> getAllSelfHrefs(BrevtypeResource brevtype) {
+        Stream.Builder<String> builder = Stream.builder();
+        if (!isNull(brevtype.getSystemId()) && !isEmpty(brevtype.getSystemId().getIdentifikatorverdi())) {
+            builder.add(createHrefWithId(brevtype.getSystemId().getIdentifikatorverdi(), "systemid"));
+        }
+        
+        return builder.build();
+    }
+
+    int[] hashCodes(BrevtypeResource brevtype) {
+        IntStream.Builder builder = IntStream.builder();
+        if (!isNull(brevtype.getSystemId()) && !isEmpty(brevtype.getSystemId().getIdentifikatorverdi())) {
+            builder.add(brevtype.getSystemId().getIdentifikatorverdi().hashCode());
+        }
+        
+        return builder.build().toArray();
+    }
+
+}
+
---
+++ src/main/java/no/fint/consumer/models/varseltype/VarseltypeLinker.java
@@ -0,0 +1,64 @@
+package no.fint.consumer.models.varseltype;
+
+import no.fint.model.resource.utdanning.kodeverk.VarseltypeResource;
+import no.fint.model.resource.utdanning.kodeverk.VarseltypeResources;
+import no.fint.relations.FintLinker;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.Objects.isNull;
+import static org.springframework.util.StringUtils.isEmpty;
+
+@Component
+public class VarseltypeLinker extends FintLinker<VarseltypeResource> {
+
+    public VarseltypeLinker() {
+        super(VarseltypeResource.class);
+    }
+
+    public void mapLinks(VarseltypeResource resource) {
+        super.mapLinks(resource);
+    }
+
+    @Override
+    public VarseltypeResources toResources(Collection<VarseltypeResource> collection) {
+        return toResources(collection.stream(), 0, 0, collection.size());
+    }
+
+    @Override
+    public VarseltypeResources toResources(Stream<VarseltypeResource> stream, int offset, int size, int totalItems) {
+        VarseltypeResources resources = new VarseltypeResources();
+        stream.map(this::toResource).forEach(resources::addResource);
+        addPagination(resources, offset, size, totalItems);
+        return resources;
+    }
+
+    @Override
+    public String getSelfHref(VarseltypeResource varseltype) {
+        return getAllSelfHrefs(varseltype).findFirst().orElse(null);
+    }
+
+    @Override
+    public Stream<String> getAllSelfHrefs(VarseltypeResource varseltype) {
+        Stream.Builder<String> builder = Stream.builder();
+        if (!isNull(varseltype.getSystemId()) && !isEmpty(varseltype.getSystemId().getIdentifikatorverdi())) {
+            builder.add(createHrefWithId(varseltype.getSystemId().getIdentifikatorverdi(), "systemid"));
+        }
+        
+        return builder.build();
+    }
+
+    int[] hashCodes(VarseltypeResource varseltype) {
+        IntStream.Builder builder = IntStream.builder();
+        if (!isNull(varseltype.getSystemId()) && !isEmpty(varseltype.getSystemId().getIdentifikatorverdi())) {
+            builder.add(varseltype.getSystemId().getIdentifikatorverdi().hashCode());
+        }
+        
+        return builder.build().toArray();
+    }
+
+}
+
